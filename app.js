b82d9140a647430826c53964b36ddcf67a771061
dez.osk@gmail.com <dez.osk@gmail.com>
2020-02-01 23:37:11 +0000

    Final

--- app.js
+++ app.js
@@ -53,8 +53,8 @@ var vm2 = new Vue({
         ]
     },
     methods: {
-        capacityChanged: function() {
-            vm1.eventCapacity = 15
+        changeTitle: function() {
+            this.$refs.name.innerText = 'Guest Book'
         }
     },
 })
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Guest List 
+
+Simple small guest list webapp as a playground learning Vue,js
+
+  - Simple layout
+  - Only 3 files
+  - Reactive
